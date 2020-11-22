04a678ccf04c2b37a905c0e3e28671c33acec907
samcaspus <sandeepguptan1998@gmail.com>
2020-11-22 13:51:23 +0530

    modified the port system

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const path = require("path");
 const router = express.Router();
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 
 app.set("view engine", "ejs");
